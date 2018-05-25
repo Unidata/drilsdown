7d4f477c9567f5d9da9e8d2b6c9390e34c66af8a
suvarchal <suvarchal.kumar@gmail.com>
2018-05-25 13:56:12 +0200

    comment out direct pulls from unidata/drilsdown and commits

--- ci/update_docs.sh
+++ ci/update_docs.sh
@@ -13,26 +13,33 @@ make html
 cd _build/html
 tar czf ~/html.tgz .
 cd ../../..
+
+
+#######
+#Commenting for now. 1)causes failues in forks; 2) team member can wrongly commit from elsewhere to 
+#drilsdown master gh-pages
+#would checkout go wrong after travis checks and then commits to github?
+#######
 # checkout doc branch
-git checkout gh-pages
-git pull git@github.com:Unidata/drilsdown.git gh-pages --allow-unrelated-histories -Xtheirs
+#git checkout gh-pages
+#git pull git@github.com:Unidata/drilsdown.git gh-pages --allow-unrelated-histories -Xtheirs
 
 # make sure the checkout was successful
-current_branch=$(git branch | grep \* | cut -d ' ' -f2-)
-if [ $current_branch = "gh-pages" ]; then
-    # clear out old docs
-    git rm -rf .
-    rm -fr docs
-    rm -fr .idea
+#current_branch=$(git branch | grep \* | cut -d ' ' -f2-)
+#if [ $current_branch = "gh-pages" ]; then
+#    # clear out old docs
+#    git rm -rf .
+#    rm -fr docs
+#    rm -fr .idea
+#
+#    tar xzf ~/html.tgz
+#    # commit and push new docs
+#    git add .
+#    git commit -a -m "publish the docs"
+#    git push origin gh-pages
 
-    tar xzf ~/html.tgz
-    # commit and push new docs
-    git add .
-    git commit -a -m "publish the docs"
-    git push origin gh-pages
-
-    git checkout master
-    else
-    echo "Did not successfully checkout gh-pages branch."
-    echo "Do you have uncommitted changes on your current branch?"
-fi
+#    git checkout master
+#    else
+#    echo "Did not successfully checkout gh-pages branch."
+#    echo "Do you have uncommitted changes on your current branch?"
+#fi
