e8a461e4bfecec833acd091d2ec35431612c47e2
suvarchal <suvarchal.kumar@gmail.com>
2019-12-12 23:11:22 +0100

    manually update docs as auth to auto commit fails from travis unidata account

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '405aaf0',
+    VERSION: 'v2.4.91-5-g5d08840',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
